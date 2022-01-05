458163d736615ad1ce8cb906115f04e126dd3562
Nathan Sadala <bernynath@gmail.com>
2022-01-05 08:24:47 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+
+app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+
+app.get('/number', async (req, res) => {
+    let number = parseInt(0 + Math.random() * 100)
+
+    res.status(200).json({
+        "number": number
+    })
+})
+
+/**
+ * body: json object with number and multiplier params
+ */
+app.post('/multiply', async (req, res) => {
+    let body = req.body
+    let output = body.number * body.multiplier
+
+    res.status(200).json({
+        "result": output
+    })
+})
+
+
+app.listen(5000, () => {
+    console.log(`server listen at http://localhost:5000`)
+})
